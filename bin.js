0ec7653e576d027b530aed1f535fbe299b2b1a18
Tobias Davis <accounts@davistobias.com>
2015-12-03 01:04:51 -0600

    Okay well I was wrong

--- bin.js
+++ bin.js
@@ -2,9 +2,8 @@
 
 var base64 = require('base64-url')
 
-var runner = process.argv[0]
-var command = process.argv[1]
-var input = process.argv[2]
+var command = process.argv[2]
+var input = process.argv[3]
 
 function commandIsAllowed(c) {
 	return c === 'encode' || c === 'decode' || c === 'escape' || c === 'unescape'
@@ -12,8 +11,8 @@ function commandIsAllowed(c) {
 
 if (!command || !input || !commandIsAllowed(command)) {
 	console.log('Missing required parameters. Run like:')
-	console.log(runner + ' [encode|decode|escape|unescape] [input]')
-	console.log('E.g., run this: ' + runner + ' decode Tm9kZS5qcyBpcyBhd2Vzb21lLg')
+	console.log('base64url [encode|decode|escape|unescape] [input]')
+	console.log('E.g., run this: base64url decode Tm9kZS5qcyBpcyBhd2Vzb21lLg')
 } else {
 	console.log(base64[command](input))
 }
