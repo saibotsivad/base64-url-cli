e51f1667b87c064d3e65397aea1f1ad323053276
Tobias Davis <accounts@davistobias.com>
2015-12-03 18:21:19 -0600

    Oh man you can test command line stuff with tape? Awesome!

---
+++ test.js
@@ -0,0 +1,26 @@
+var test = require('tape')
+var spawn = require('tape-spawn')
+
+test('test encode', function (t) {
+	var st = spawn(t, 'node bin.js encode "Node.js is awesome."')
+	st.stdout.match(/Tm9kZS5qcyBpcyBhd2Vzb21lLg/)
+	st.end()
+})
+
+test('test decode', function (t) {
+	var st = spawn(t, 'node bin.js decode "Tm9kZS5qcyBpcyBhd2Vzb21lLg"')
+	st.stdout.match(/Node\.js is awesome\./)
+	st.end()
+})
+
+test('test escape', function (t) {
+	var st = spawn(t, 'node bin.js escape "This+is/goingto+escape=="')
+	st.stdout.match(/This-is_goingto-escape/)
+	st.end()
+})
+
+test('test decode', function (t) {
+	var st = spawn(t, 'node bin.js unescape "This-is_goingto-escape"')
+	st.stdout.match(/This\+is\/goingto\+escape==/)
+	st.end()
+})
