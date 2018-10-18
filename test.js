dc4dc7d521d9d5a532787bde55a1e1dd607497df
Tobias Davis <accounts@davistobias.com>
2018-10-18 13:17:57 -0500

    change: use arrow functions mmm tasty

--- test.js
+++ test.js
@@ -1,25 +1,25 @@
 const test = require('tape')
 const spawn = require('tape-spawn')
 
-test('test encode', function (t) {
+test('test encode', t => {
 	const st = spawn(t, 'node bin.js encode "Node.js is awesome."')
 	st.stdout.match(/Tm9kZS5qcyBpcyBhd2Vzb21lLg/)
 	st.end()
 })
 
-test('test decode', function (t) {
+test('test decode', t => {
 	const st = spawn(t, 'node bin.js decode "Tm9kZS5qcyBpcyBhd2Vzb21lLg"')
 	st.stdout.match(/Node\.js is awesome\./)
 	st.end()
 })
 
-test('test escape', function (t) {
+test('test escape', t => {
 	const st = spawn(t, 'node bin.js escape "This+is/goingto+escape=="')
 	st.stdout.match(/This-is_goingto-escape/)
 	st.end()
 })
 
-test('test decode', function (t) {
+test('test decode', t => {
 	const st = spawn(t, 'node bin.js unescape "This-is_goingto-escape"')
 	st.stdout.match(/This\+is\/goingto\+escape==/)
 	st.end()
